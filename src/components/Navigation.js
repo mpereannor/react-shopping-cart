037e7e76c54e6a96389d4b81aeb5f9fe60e6d349
chudnofskyATD <mpereannor@gmail.com>
2019-10-07 19:13:13 +0000

    navigation.js tested with other 2 methods of consuming: contextHook works, contextConsumer works, static Context Type doesn't

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
-import {useContext} from 'react';
 import CartContext from '../contexts/CartContext';
 
 
+//using ContextHook
+
 const Navigation = () => {
-const{cart} = useContext(CartContext);
+const cart = useContext(CartContext);
 
 	return (
 		<div className="navigation">
@@ -16,5 +17,52 @@ const{cart} = useContext(CartContext);
 		</div>
 	);
 };
+
+//export default Navigation
+
+//using ContextConsumer 
+
+// const Navigation = () => {
+	
+// 	return (
+// 		<CartContext.Consumer>
+// 			{
+// 				props => {
+// 					return (
+// 						<div className="navigation">
+// 							<NavLink to="/">Products</NavLink>
+// 							<NavLink to="/cart">
+// 								Cart <span>{props.length}</span>
+// 							</NavLink>
+// 						</div>
+// 					)
+// 				}
+				
+// 			}
+			
+// 		</CartContext.Consumer>
+// 	)
+// }
+// export default Navigation;
+
+//using static Context Type 
+
+// const Navigation = () => {
+	
+// 	return (
+// 		<div className="navigation">
+// 						<NavLink to="/">Products</NavLink>
+// 						<NavLink to="/cart">
+// 							Cart <span>{this.cart.length}</span>
+//  							</NavLink>
+// 					</div>
+
+// 	)
+
+// }
+
+// Navigation.contextType = CartContext;
+
+//error using static Context Type; this/cart cannot defined 
 
 export default Navigation;
